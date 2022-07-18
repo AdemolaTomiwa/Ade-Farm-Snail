242b844baba8179d7569f1851fb4add67d7ea7a7
thetommedia@gmail.com <thetommedia@gmail.com>
2022-07-18 17:33:56 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRoute from './routes/userRoute.js';
 import orderRoute from './routes/orderRoute.js';
 import passwordResetRoute from './routes/passwordReset.js';
 import uploadRoute from './routes/uploadRoute.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -29,6 +30,19 @@ app.use('/api/users', userRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/uploads', uploadRoute);
 app.use('/api/password-reset', passwordResetRoute);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+   app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+   );
+} else {
+   app.get('/', (req, res) => {
+      res.send('API is running...');
+   });
+}
 
 const PORT = process.env.PORT || 5000;
 
